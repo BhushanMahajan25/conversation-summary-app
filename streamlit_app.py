7e479c68c5c3973af5f89d38fbe23994dc085371
Bhushan Mahajan <bhushan@Bhushans-MacBook-Air.local>
2023-12-20 10:57:24 -0500

    initial commit

---
+++ streamlit_app.py
@@ -0,0 +1,110 @@
+import torch
+import streamlit as st
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, AutoModelForCausalLM
+
+@st.cache_resource
+def get_model():
+    original_model_name='google/flan-t5-base'
+    my_model_name = 'BhushanM25/fine-tuned-flan-t5-base-v4'
+    tokenizer = AutoTokenizer.from_pretrained(original_model_name)
+    
+    my_model = AutoModelForSeq2SeqLM.from_pretrained(original_model_name, torch_dtype=torch.bfloat16)
+    my_model.load_adapter(my_model_name)
+    return tokenizer, my_model
+
+tokenizer, model = get_model()
+
+def get_summary(conversation):
+    prompt = f"""
+Summarize the following conversation.
+
+{conversation}
+
+Summary:
+"""
+
+    inputs = tokenizer(prompt, return_tensors='pt')
+    output = tokenizer.decode(
+        model.generate(
+            inputs["input_ids"],
+            max_new_tokens=200,
+        )[0],
+        skip_special_tokens=True
+    )
+    print("Generated fine-tuned output: ", output)
+    return output
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+    
+if 'current_user' not in st.session_state:
+    st.session_state['current_user'] = "user1"
+    
+# Create placeholders for chat messages
+message_placeholders = {}
+
+def convert_user_to_person(user):
+    return "#Person1#" if user == "user1" else "#Person2#"
+
+def toggle_user(current_user):
+    # Toggle user logic
+    if current_user == "user1":
+        st.session_state['current_user'] = "user2"
+    else:
+        st.session_state['current_user'] = "user1"
+        
+def format_messages():
+     # Collect messages from current session
+    session_messages = st.session_state.get("messages")
+    print("session_messages: ", session_messages)
+    messages = []
+    for message_dict in session_messages:
+        person = "#Person1#" if message_dict["role"] == "user1" else "#Person2#"
+        content = message_dict["content"]
+        message = person + ": " + content + "\n"
+        messages.append(message)
+    return "".join(messages)
+
+
+st.title("Conversation Summarization App")
+
+# # Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    role = message["role"]
+    content = message["content"]
+    # message_placeholders[role] = st.empty()
+    with st.chat_message(role[-1]):
+        st.markdown(content)
+
+user_input = st.chat_input("Say something")
+
+if user_input:
+    
+    current_user = st.session_state['current_user'] 
+    print("CURRENT_USER: ", current_user)
+        
+    st.session_state.messages.append({"role": current_user, "content": user_input})
+    # Display user message in chat message container
+    with st.chat_message(current_user[-1]):
+        st.markdown(f'{current_user} says - {user_input}')
+        
+    toggle_user(current_user)
+    
+
+# Button to trigger summarization
+if st.button("Summarize"):
+
+    # Format messages as input prompt 
+    # (Placeholder - you said you will handle this part)
+    formatted_input = format_messages() 
+    print("formatted_input: ", formatted_input)
+
+    # Pass input to model for summarization
+    summary_text = get_summary(formatted_input)  
+
+    # Clear session history
+    st.session_state.get("messages").clear()
+
+    # Display summary  
+    st.text_area("Summary:", f" Summary:\n{summary_text}", height=100)
